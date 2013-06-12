eb5f95e87f3d64f2a691c0a8cdc1d3740ff40d71
Ken Lu <ken@seeice.com>
2013-06-12 08:39:01 -0400

    frist version of shopping cart

--- public/basic/controller.js
+++ public/basic/controller.js
@@ -1,3 +1,13 @@
 function SayController($scope) {
   $scope.greeting = { text: 'Say'}
 }
+function CartController($scope) {
+    $scope.items = [
+    {title: 'Paint pots', quantity: 8, price: 3.95},
+    {title: 'Polka dots', quantity: 17, price: 12.95},
+    {title: 'Pebbles', quantity: 5, price: 6.95}
+    ];
+    $scope.remove = function(index) {
+        $scope.items.splice(index, 1);
+    }
+}
