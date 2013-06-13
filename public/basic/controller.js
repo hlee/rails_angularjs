53b9434299c1eb0628a377312be548aa4539dd13
Ken Lu <ken@seeice.com>
2013-06-13 08:36:03 -0400

    function of toggle menu which is not working FIXME

--- public/basic/controller.js
+++ public/basic/controller.js
@@ -1,13 +1,24 @@
 function SayController($scope) {
   $scope.greeting = { text: 'Say'}
 }
+
 function CartController($scope) {
     $scope.items = [
     {title: 'Paint pots', quantity: 8, price: 3.95},
     {title: 'Polka dots', quantity: 17, price: 12.95},
     {title: 'Pebbles', quantity: 5, price: 6.95}
     ];
+
     $scope.remove = function(index) {
         $scope.items.splice(index, 1);
     }
+}
+
+function DeathrayMenuController($scope) {
+    $scope.menu = {text: 'Stun'};
+    $scope.menuState.show = false;
+    $scope.toggleMenu = function() {
+        $scope.menuState.show = !$scope.menuState.show;
+    };
+    // death ray functions left as exercise to reader
 }
