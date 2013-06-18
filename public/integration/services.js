a88ecc0daa8b50a0115f619460c2211320ed6569
Ken Lu <ken@seeice.com>
2013-06-18 07:53:21 -0400

    check in however not work quit well

--- public/integration/services.js
+++ public/integration/services.js
@@ -1,21 +1,21 @@
 var services = angular.module('guthub.services', ['ngResource']);
 services.factory('Recipe', ['$resource', function($resource) {
-return $resource('/recipes/:id', {id: '@id'}); }]);
+  return $resource('/recipes/:id', {id: '@id'}); }]);
 services.factory('MultiRecipeLoader', ['Recipe', '$q', function(Recipe, $q) {
-return function() {
-var delay = $q.defer(); Recipe.query(function(recipes) {
-delay.resolve(recipes); }, function() {
-          delay.reject('Unable to fetch recipes');
-        });
-return delay.promise; };
+  return function() {
+    var delay = $q.defer(); Recipe.query(function(recipes) {
+      delay.resolve(recipes); }, function() {
+        delay.reject('Unable to fetch recipes');
+      });
+    return delay.promise; };
 }]);
 services.factory('RecipeLoader', ['Recipe', '$route', '$q', function(Recipe, $route, $q) {
-return function() {
-var delay = $q.defer();
-Recipe.get({id: $route.current.params.recipeId}, function(recipe) {
-delay.resolve(recipe);
-}, function() {
-delay.reject('Unable to fetch recipe ' + $route.current.params.recipeId);
-});
-return delay.promise; };
+  return function() {
+    var delay = $q.defer();
+    Recipe.get({id: $route.current.params.recipeId}, function(recipe) {
+      delay.resolve(recipe);
+    }, function() {
+      delay.reject('Unable to fetch recipe ' + $route.current.params.recipeId);
+    });
+    return delay.promise; };
 }]);
